e9f66a0b946bda962b6828a381cbb42a44141325
zukaberidze <zaqaria.beridze@sangu.edu.ge>
2020-04-25 22:30:34 +0400

    Add project files.

---
+++ ListCopy/MyList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListCopy
+{
+    class MyList<T> : IEnumerator
+    {
+        public List<T> list = new List<T>();
+
+        public void AddNumber(T num)
+        {
+            list.Add(num);
+        }
+    }
+
+    internal interface IEnumerator
+    {
+    }
+}
---
+++ ListCopy/Program.cs
@@ -0,0 +1,21 @@
+﻿using ListCopy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListCopy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var N1 = new MyList<int>();
+            N1.AddNumber(1);
+            N1.AddNumber(2);
+            N1.AddNumber(3);
+
+            var N2 = new MyList<int>();
+          
+        }
+    }
+}
